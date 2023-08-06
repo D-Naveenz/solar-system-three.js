d29bab7cd5bbbd868129d718378d785493eb30b4
Naveen Dharmathunga <dasheenaveen@outlook.com>
2023-08-06 18:57:32 +0530

    decluttered

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { RouterView } from 'vue-router'
+import { setStyle } from '@/utils/theme';
+
 import NavbarComponent from '@/components/NavbarComponent.vue';
 
 // data
@@ -24,23 +26,6 @@ function themeProps(theme: string) {
     '--color-border-hover': `var(--vt-c-divider-${lightName}-1)`,
     '--color-heading': `var(--vt-c-text-${lightName}-1)`,
     '--color-text': `var(--vt-c-text-${lightName}-2)`,
-  }
-}
-
-function setStyle(elment: HTMLElement, css: string | { [key: string]: string }) {
-  if (css instanceof String) {
-    // split the css string into an array of properties
-    var properties = css.split(/[;\n]/gm).filter((v) => v !== '')
-    // split each property into an array of [property, value]
-    var propArray = properties.map((v) => v.split(/[:\s]/gm).filter((v) => v !== ''))
-    // set the style
-    propArray.forEach((v) => elment.style.setProperty(v[0], v[1]))
-  }
-  else {
-    // set the style
-    Object.keys(css).forEach((key) => {
-      elment.style.setProperty(key, css[key])
-    })
   }
 }
 </script>
--- src/components/ThreeController.vue
+++ src/components/ThreeController.vue
@@ -1,32 +1,28 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import createTerrain from '@/3d-project/components/terrain'
 import { SceneController } from '@/3d-project/scene-controller'
+
+function initScene() {
+  // Get the reference to the canvas element
+  const canvas = document.querySelector('#scene-3d')
+  // Create an instance of 3D scene
+  const world = new SceneController(canvas)
+  // Start the animation loop
+  world.animationLoop.start()
+
+  let terrain = createTerrain({ color: 'green' })
+  world.addObject(terrain)
+}
+
+onMounted(() => {
+  initScene()
+})
 </script>
 
 <template>
   <canvas id="scene-3d"></canvas>
 </template>
-
-<script lang="ts">
-export default {
-  methods: {
-    initScene() {
-      // Get the reference to the canvas element
-      const canvas = document.querySelector('#scene-3d')
-      // Create an instance of 3D scene
-      const world = new SceneController(canvas)
-      // Start the animation loop
-      world.animationLoop.start()
-
-      let terrain = createTerrain({ color: 'green' })
-      world.addObject(terrain)
-    }
-  },
-  mounted() {
-    this.initScene()
-  }
-}
-</script>
 
 <style scoped>
 #scene-3d {
