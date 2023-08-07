da641632ececa441e17aa12357072b686a19fbca
Naveen Dharmathunga <dasheenaveen@outlook.com>
2023-08-07 23:02:56 +0530

    Added stars skybox

--- src/components/ThreeController.vue
+++ src/components/ThreeController.vue
@@ -1,16 +1,46 @@
 <script setup lang="ts">
 import { onBeforeMount, onMounted, ref } from 'vue'
-import createTerrain from '@/3d-project/components/terrain'
 import { SceneController } from '@/3d-project/scene-controller'
 import type { SceneProperties } from '@/3d-project/types/scene-props'
+import { AmbientLight, BufferAttribute, BufferGeometry, PointLight, Points, PointsMaterial, TextureLoader } from 'three';
 
 // Data
 const heightCutoff = ref(0)
 
 function createGameObjects(controller: SceneController) {
-  // Create a terrain
-  let terrain = createTerrain({ color: 'green' })
-  controller.dir.add("terrain", {object3D: terrain})
+  // ambient light
+  let ambientLight = new AmbientLight(0xffffff, 2)
+  ambientLight.position.set(0, 0, 0)
+  controller.dir.add("ambientLight", {object3D: ambientLight})
+
+  // point light
+  let pointLight = new PointLight(0xffffff, 2)
+  pointLight.position.set(0, 0, 0)
+  controller.dir.add("pointLight", {object3D: pointLight})
+
+  // particles
+  const particleGeometry = new BufferGeometry
+  const particleCount = 5000
+
+  const positions = new Float32Array(particleCount * 3) // 3 vertices per particle
+  // loop through each particle and set their random position
+  for (let i = 0; i < particleCount * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 100 // random position between -50 and 50
+  }
+
+  particleGeometry.setAttribute('position', new BufferAttribute(positions, 3)) // 3 vertices per particle (x, y, z)
+
+  const texturwLoader = new TextureLoader()
+  const particleTexture = texturwLoader.load('./assets/textures/star.png')
+
+  const particleMaterial = new PointsMaterial({
+    size: 0.15,
+    map: particleTexture,
+    sizeAttenuation: true // make the particles smaller when they are further away
+  })
+
+  const particles = new Points(particleGeometry, particleMaterial)
+  controller.dir.add("particles", {object3D: particles})
 }
 
 onBeforeMount(() => {
